80ecb8075e3536cfa3d0cd627e7c6f99e27c9299
rares-tudor <rarestudor04@gmail.com>
2020-06-05 20:22:55 +0200

    Fixed bugs in open command. Bugs concerning the comparison of balance have to be fixed.

--- commands/open.js
+++ commands/open.js
@@ -1,10 +1,7 @@
-const {role_id} = require ('./config.json');
-
 module.exports = {
     name: 'open',
     description: 'Opens the stock market manually.',
     execute(msg) {
-        var response = "<@&";
-        response.concat(role_id, "> The stock market has been opened.");
+        msg.channel.send("<@&709405462831038514> The stock market has been opened.");
     }
 }
